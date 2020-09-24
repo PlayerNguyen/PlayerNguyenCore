368b18b54eb037bee72d8da313e229707fdce88e
PlayerNguyen <daudaua3782@gmail.com>
2020-09-24 23:26:35 +0700

    Update command implementation system

--- src/main/java/com/playernguyen/playernguyencore/command/CoreExecutorManager.java
+++ src/main/java/com/playernguyen/playernguyencore/command/CoreExecutorManager.java
@@ -1,9 +1,23 @@
 package com.playernguyen.playernguyencore.command;
 
 import com.playernguyen.playernguyencore.manager.CoreManagerSet;
+import com.playernguyen.playernguyencore.utils.CorePreconditions;
+import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 
 public class CoreExecutorManager extends CoreManagerSet<CoreCommandExecutor> {
 
+    @Override
+    public void add(CoreCommandExecutor item) {
+        super.add(item);
+        // Register command
+        PluginCommand command = Bukkit.getPluginCommand(item.getCommand());
+        // Check null
+        CorePreconditions.notNull(command,
+                "Command not register, please config in plugin.yml -> commands");
+        // Set executor to item
+        command.setExecutor(item);
+    }
 
 
 }
